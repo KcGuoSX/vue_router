0e10ff9fd6ce9b8389175495831152b65a1d78c0
chris <chris@aladinfun.com>
2023-05-04 16:31:34 +0800

    fix: router

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <router-link to="/">Home</router-link>
+    <router-link to="/login">Login</router-link>
+
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
